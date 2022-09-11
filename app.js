5ef52809c008a9f4e6ab71461224b117e2cdd131
Ahmed <Ahmedshams22@gmail.com>
2022-09-11 22:26:41 +0200

    Almost finish project done with add menuBar add active class and add smooth transition

--- app.js
+++ app.js
@@ -22,8 +22,8 @@
  * Define Global Variables
  * 
 */
-
-
+  const ulEl = document.querySelector('#navbar__list');
+  const sections = document.querySelectorAll('section');
 /**
  * End Global Variables
  * Start Helper Functions
@@ -37,9 +37,34 @@
  * Begin Main Functions
  * 
 */
+    function scrolToSection(e){
+      for (let i = 0 ; i < sections.length ; i++){
+           sections[i].classList.remove('active');  
+      }
+
+     for (let i = 0 ; i < sections.length ; i++){
+       //debugger;
+       if (e.target.textContent === sections[i].id){
+          sections[i].scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
+          sections[i].classList.toggle('active');
+        }   
+       
+     }
+    }
 
 // build the nav
 
+ for (let i =1 ; i <=4 ; i++){
+  const navEl = document.createElement('li');
+  navEl.textContent= "Section" + i;
+  navEl.classList.add('menu__link');
+
+  navEl.addEventListener('click',function(e){
+    scrolToSection(e);
+   
+  })
+  ulEl.appendChild(navEl);
+ }
 
 // Add class 'active' to section when near top of viewport
 
